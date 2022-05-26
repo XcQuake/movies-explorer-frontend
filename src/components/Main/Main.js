5e6f8276c6b09a806708f1c9028d28bc0a3bbd37
XcQuid <creed.35@yandex.ru>
2022-05-26 19:12:45 +0300

    feat: add AboutProject component markup

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,10 +1,12 @@
 import './Main.css';
-import Promo from '../Promo/Promo'
+import Promo from '../Promo/Promo';
+import AboutProject from '../AboutProject/AboutProject';
 
 function Main() {
   return (
     <section className='main'>
       <Promo />
+      <AboutProject />
     </section>
   );
 }
