68cbc5e3b3c3bf63593be18edb0cba79723ca9f8
XcQuid <creed.35@yandex.ru>
2022-05-29 19:31:00 +0300

    feat: add Techs component markup

---
+++ src/components/Techs/Techs.js
@@ -0,0 +1,26 @@
+import './Techs.css';
+
+function Techs() {
+  return (
+    <section className='techs'>
+      <div className='techs__wrapper'>
+        <h2 className='techs__title'>Технологии</h2>
+        <h3 className='techs__subtitle'>7&nbsp;технологий</h3>
+        <p className='techs__paragraph'>
+          На&nbsp;курсе веб-разработки мы&nbsp;освоили технологии, которые применили в&nbsp;дипломном проекте.
+        </p>
+        <ui className='techs__techlist'>
+          <li className='techs__tech'>HTML</li>
+          <li className='techs__tech'>CSS</li>
+          <li className='techs__tech'>JS</li>
+          <li className='techs__tech'>React</li>
+          <li className='techs__tech'>Git</li>
+          <li className='techs__tech'>Express.js</li>
+          <li className='techs__tech'>mongoDB</li>
+        </ui>
+      </div>
+    </section>
+  );
+}
+
+export default Techs;
