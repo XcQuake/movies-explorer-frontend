3c4da4783c107148c28437a7486f869779d3d0df
XcQuid <creed.35@yandex.ru>
2022-05-31 14:04:39 +0300

    feat: create Movies component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,23 @@
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
 import Header from '../Header/Header';
 import Main from '../Main/Main';
+import Movies from '../Movies/Movies';
 import Footer from '../Footer/Footer';
+
 
 export default function App() {
   return (
     <>
       <Header />
-      <Main />
+      <Switch>
+        <Route path='/movies'>
+          <Movies />
+        </Route>
+        <Route exact path='/'>
+          <Main />
+        </Route>
+      </Switch>
       <Footer />
     </>
   );
