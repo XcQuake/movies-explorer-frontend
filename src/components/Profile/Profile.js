6fee072b98a71e73d7fb9bad30a8a808909be6d9
XcQuid <creed.35@yandex.ru>
2022-06-06 16:20:57 +0300

    refactor: change Profile component

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,31 +1,68 @@
+import { useRef, useState } from 'react';
 import './Profile.css';
 
 function Profile() {
-  const user = {
-    name: 'Виталий',
-    email: 'pochta@yandex.ru',
+  const [isEdited, setIsEdited] = useState(false);
+  const [name, setName] = useState('Виталий');
+  const [email, setEmail] = useState('pochta@yandex.ru');
+  const nameInputRef = useRef(false);
+
+  function handleNameChange(evt) {
+    evt.preventDefault();
+    setName(evt.target.value);
   };
+
+  function handleEmailChange(evt) {
+    evt.preventDefault();
+    setEmail(evt.target.value);
+  }
+
+  async function handleEditButtonClick(evt) {
+    evt.preventDefault();
+    await setIsEdited(true);
+    nameInputRef.current.focus();
+  };
+
+  function handleSaveButtonClick(evt) {
+    evt.preventDefault();
+    setIsEdited(false);
+  }
 
   return (
     <main className='profile'>
       <div className='profile__wrapper'>
-        <div className='profile__container'>
-          <h2 className='profile__greeting'>Привет, {user.name}!</h2>
-          <ul className='profile__info'>
-            <li className='profile__info-field'>
-              <span className='profile__info-title'>Имя</span>
-              <p className='profile__info-value'>{user.name}</p>
-            </li>
-            <li className='profile__info-field'>
-              <span className='profile__info-title'>E-mail</span>
-              <p className='profile__info-value'>{user.email}</p>
-            </li>
-          </ul>
-        </div>
-        <div className='profile__navbar'>
-          <button className='profile__button'>Редактировать</button>
-          <button className='profile__button' style={{ color: '#FF3055' }}>Выйти из&nbsp;аккаунта</button>
-        </div>
+        <h2 className='profile__greeting'>Привет, {name}!</h2>
+        <form className='profile__info'>
+          <fieldset className='profile__info-fieldset'>
+            <label className='profile__info-field'>
+              <span className='profile__label'>Имя</span>
+              <input className='profile__input'
+                name='name'
+                value={name}
+                onChange={handleNameChange}
+                ref={nameInputRef}
+                disabled={!isEdited}
+              />
+            </label>
+            <label className='profile__info-field'>
+              <span className='profile__label'>E-mail</span>
+              <input
+                name='email'
+                className='profile__input'
+                value={email}
+                onChange={handleEmailChange}
+                disabled={!isEdited}
+              />
+            </label>
+          </fieldset>
+          <div className='profile__controls'>
+            { isEdited
+                ? <button className='profile__button profile__button_save' onClick={handleSaveButtonClick}>Сохранить</button>
+                : <button className='profile__button' onClick={handleEditButtonClick}>Редактировать</button>
+            }
+            { !isEdited && <button className='profile__button profile__button_exit'>Выйти из&nbsp;аккаунта</button> }
+          </div>
+        </form>
       </div>
     </main>
   );
