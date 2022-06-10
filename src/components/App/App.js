6ddddda45a776367d8963c856546a71dacf1c1a3
XcQuid <creed.35@yandex.ru>
2022-06-10 16:35:46 +0300

    feat: add different routes in App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -9,35 +9,45 @@ import SearchForm from '../SearchForm/SearchForm';
 import Profile from '../Profile/Profile';
 import Login from '../Login/Login';
 import Register from '../Register/Register';
+import Navigation from '../Navigation/Navigation';
+import { useState } from 'react';
 
 export default function App() {
+  const [isNavigationOpen, setIsNavigationOpen] = useState(false);
+
+  function handleBurgerClick() {
+    setIsNavigationOpen(!isNavigationOpen);
+  };
+
   return (
     <>
+      <Route path='/signin' component={Login} />
+      <Route path='/signup' component={Register} />
+      <Route path='/(movies|saved-movies|profile)'>
+        <Header
+          onBurgerClick={handleBurgerClick}
+        />
+      </Route>
+      <Route path='(movies|saved-movies)' component={SearchForm} />
       <Switch>
         <Route path='/movies'>
-          <Header />
           <SearchForm />
           <Movies />
-          <Footer />
         </Route>
         <Route path='/saved-movies'>
-          <Header />
           <SearchForm />
           <SavedMovies />
-          <Footer />
         </Route>
         <Route path='/profile'>
-          <Header />
           <Profile />
         </Route>
-        <Route path='/signin' component={Login} />
-        <Route path='/signup' component={Register} />
         <Route exact path='/'>
           <Header />
           <Main />
-          <Footer />
         </Route>
       </Switch>
+      <Route path='/(|movies|saved-movies|profile)' component={Footer} />
+      <Navigation isOpen={isNavigationOpen} />
     </>
   );
 }
