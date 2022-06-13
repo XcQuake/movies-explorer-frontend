1dbcfcdac83af9e5a69e8c21a94074417eecf6ff
XcQuid <creed.35@yandex.ru>
2022-06-13 10:32:16 +0300

    fix: swap links in AuthBar

--- src/components/AuthBar/AuthBar.js
+++ src/components/AuthBar/AuthBar.js
@@ -4,8 +4,8 @@ import './AuthBar.css';
 function AuthBar() {
   return (
     <div className='authbar'>
-      <Link to='/signin' className='authbar__button authbar__button_signin'>Войти</Link>
       <Link to='/signup' className='authbar__button authbar__button_signup'>Регистрация</Link>
+      <Link to='/signin' className='authbar__button authbar__button_signin'>Войти</Link>
     </div>
   );
 }
