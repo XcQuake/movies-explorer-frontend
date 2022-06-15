af3ffa9b950b5bcd19c55e3aee5ebd85839238f8
XcQuid <creed.35@yandex.ru>
2022-06-15 18:10:25 +0300

    feat: add temporary validation

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,20 +1,65 @@
 import './Register.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Logo from '../Logo/Logo';
 import AuthForm from '../AuthForm/AuthForm';
+import { useFormWithValidation } from '../../hooks/useFormWithValidation';
+import { useValidation } from '../../hooks/useValidation';
 
 function Register() {
   const [errorMessage, setErrorMessage] = useState('');
+  const [values, setValues] = useState({
+    username: '',
+    email: '',
+    password: '',
+  });
+  const [isValid, setIsValid] = useState(false);
+  const [isTouched, setIsTouched] = useState({
+    username: false,
+    email: false,
+    password: false,
+  })
 
   function submitHandler(evt) {
     evt.preventDefault()
     setErrorMessage('Пока ничего не работает')
+    console.log('pepega')
   };
 
   const textConfig = {
     button: 'Зарегистрироваться',
     linkText: 'Уже зарегистрированы?',
     link: 'Войти',
+  };
+
+  const usernameValidate = useValidation(values.username, {
+    minLength: 2,
+    isRequired: true,
+    isUsername: true,
+  });
+
+  const emailValidate = useValidation(values.email, {
+    minLength: 5,
+    isRequired: true,
+    isEmail: true,
+  });
+
+  useEffect(() => {
+    setIsValid(usernameValidate.validity && emailValidate.validity);
+  }, [usernameValidate.validity, emailValidate.validity]);
+
+  const handleChangeInput = (e) => {
+    e.preventDefault();
+    const target = e.target;
+    const name = target.name;
+    const value = target.value;
+    setValues({...values, [name]: value});
+  }
+
+  const handleTouchInput = (e) => {
+    e.preventDefault();
+    const target = e.target;
+    const name = target.name;
+    setIsTouched({...isTouched, [name]: true})
   }
 
   return (
@@ -29,17 +74,22 @@ function Register() {
           error={errorMessage}
           onSubmit={submitHandler}
           config={textConfig}
+          isValid={isValid}
         >
           <label className='authform__field'>
             Имя
             <input
               className='authform__input'
               type='text'
-              name='name'
-              minLength="5"
-              maxLength="80"
+              name='username'
+              minLength="2"
+              maxLength="20"
+              typeof=''
               required
+              onChange={handleChangeInput}
+              onBlur={handleTouchInput}
             />
+            <span className='authform__input-error'>{isTouched.username && usernameValidate.errorMessage}</span>
           </label>
           <label className='authform__field'>
             E-mail
@@ -50,7 +100,10 @@ function Register() {
               minLength="5"
               maxLength="80"
               required
+              onChange={handleChangeInput}
+              onBlur={handleTouchInput}
             />
+            <span className='authform__input-error'>{isTouched.email && emailValidate.errorMessage}</span>
           </label>
           <label className='authform__field'>
             Пароль
@@ -61,7 +114,10 @@ function Register() {
               minLength="6"
               maxLength="30"
               required
+              onChange={handleChangeInput}
+              onBlur={handleTouchInput}
             />
+            <span className='authform__input-error'>{}</span>
           </label>
         </AuthForm>
       </div>
