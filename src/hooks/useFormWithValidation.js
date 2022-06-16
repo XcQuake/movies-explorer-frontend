b6a389c8e3f85d2fa57218e906727fa728dc1724
XcQuid <creed.35@yandex.ru>
2022-06-16 16:46:57 +0300

    feat: add useFormWithValidation hook

--- src/hooks/useFormWithValidation.js
+++ src/hooks/useFormWithValidation.js
@@ -1,17 +1,51 @@
-import { useState, useCallback } from "./react";
+import { useState, useCallback, useEffect } from 'react';
 
-export function useFormWithValidation() {
-  const [values, setValues] = useState({});
-  const [errors, setErrors] = useState({});
+export function useFormWithValidation({validations, initialState, initialErrors}) {
+  const [values, setValues] = useState(initialState);
+  const [errors, setErrors] = useState(initialErrors);
   const [isValid, setIsValid] = useState(false);
 
-  const handleChange = (event) => {
-    const target = event.target;
+  const handleValidate = async (input) => {
+    const value = input.value;
+    const name = input.name;
+
+    if (validations) {
+      const newErrors = errors;
+      const validation = validations[name];
+      const pattern = validation?.pattern;
+      const minLength = validation?.minLength;
+
+      if (validation?.required?.value && !value) { // requirement check
+        setIsValid(false);
+        setErrors({...errors, [name]: validation?.required?.message});
+      } else if (pattern?.value && value && !RegExp(pattern.value).test(value)) { // pattern check
+        setIsValid(false);
+        setErrors({...errors, [name]: pattern.message});
+      } else if (minLength?.value && value && value.length < minLength.value) { // minLength check
+        setIsValid(false);
+        setErrors({...errors, [name]: minLength.message});
+      } else {
+        delete newErrors[name];
+        setErrors(newErrors);
+        return;
+      }
+    }
+  }
+
+  useEffect(() => {
+    if (Object.keys(errors).length === 0) {
+      setIsValid(true);
+    } else {
+      setIsValid(false);
+    }
+  }, [errors, values])
+
+  const handleChange = (e) => {
+    const target = e.target;
     const name = target.name;
     const value = target.value;
     setValues({...values, [name]: value});
-    setErrors({...errors, [name]: target.validationMessage });
-    setIsValid(target.closest("form").checkValidity());
+    handleValidate(target);
   };
 
   const resetForm = useCallback(
