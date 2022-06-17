3d9f2d283fffdaa92d6d2dd2743916989f74c623
XcQuid <creed.35@yandex.ru>
2022-06-17 17:24:16 +0300

    feat: create MainApi fetch functions

---
+++ src/utils/MainApi.js
@@ -0,0 +1,56 @@
+const BASE_URL = 'https://api.xcqmovies.nomoredomains.xyz';
+
+function processResult(res) {
+  if (res.ok) return res.json();
+  return Promise.reject(res.status);
+};
+
+export const signUp = (username, email, password) => {
+  return fetch(`${BASE_URL}/signup`, {
+    credentials: 'include',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify ({
+      "name": username,
+      "email": email,
+      "password": password,
+    }),
+  })
+    .then((res) =>processResult(res))
+};
+
+export const signIn = (email, password) => {
+  return fetch(`${BASE_URL}/signin`, {
+    credentials: 'include',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify ({
+      "email": email,
+      "password": password,
+    })
+  })
+  .then((res) => processResult(res))
+};
+
+export const signOut = () => {
+  return fetch(`${BASE_URL}/signout`, {
+    credentials: 'include',
+    method: 'GET'
+  })
+  .then((res) => processResult(res))
+};
+
+export const checkToken = () => {
+  return fetch(`${BASE_URL}/users/me`, {
+    credentials: 'include',
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    }
+  })
+  .then((res) => processResult(res))
+};
