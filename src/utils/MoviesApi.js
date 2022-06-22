2389afc610c5a8f53a7b5ad62e8b3c0cf654652b
XcQuid <creed.35@yandex.ru>
2022-06-22 21:16:23 +0300

    feat: create MoviesApi util

---
+++ src/utils/MoviesApi.js
@@ -0,0 +1,13 @@
+const BASE_URL = 'https://api.nomoreparties.co/beatfilm-movies';
+
+async function processResult(res) {
+  const result = await res.json();
+  return res.ok ? result : Promise.reject(result.message);
+};
+
+export const getMovies = () => {
+  return fetch(`${BASE_URL}`, {
+    method: 'GET'
+  })
+  .then((res) => processResult(res))
+}
