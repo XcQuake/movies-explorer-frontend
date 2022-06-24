58417f705a0ad3502124fc5498a161be049720c3
XcQuid <creed.35@yandex.ru>
2022-06-24 14:29:19 +0300

    feat: add api cards display in Movies component

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,19 +1,39 @@
 import './Movies.css';
+import * as MoviesApi from '../../utils/MoviesApi';
 import MoviesCardList from '../MoviesCardList/MoviesCardList';
 import MoviesCard from '../MoviesCard/MoviesCard';
 import MoreButton from '../Buttons/MoreButton/MoreButton';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 function Movies() {
+  const [movies, setMovies] = useState([]);
+
+
+  function getMovies() {
+    MoviesApi.getMovies()
+      .then((movies) => {
+        setMovies(movies);
+      })
+      .catch(err => console.log(err))
+  }
+
+  useEffect(() => {
+    getMovies();
+  }, [])
+
   return (
     <div className='movies'>
       <div className='movies__wrapper'>
         <MoviesCardList>
-          <MoviesCard />
-          <MoviesCard />
-          <MoviesCard />
-          <MoviesCard />
-          <MoviesCard />
-          <MoviesCard />
+          {movies.slice(0, 12).map(
+            (film) => (
+              <MoviesCard
+                key={film.id}
+                movieData={film}
+              />
+            ),
+          )}
         </MoviesCardList>
         <MoreButton />
       </div>
--- src/components/MoviesCard/MoviesCard.js
+++ src/components/MoviesCard/MoviesCard.js
@@ -2,20 +2,24 @@ import './MoviesCard.css';
 import moviesImage from '../../images/movie_pic.png';
 import SaveButton from '../Buttons/MovieButton/MovieButton';
 
-function MoviesCard() {
-  const movie = {
-    img: moviesImage,
-    title: 'Баския: Взрыв реальности',
-    duration: '1ч 17м',
+function MoviesCard({movieData}) {
+  const title = movieData.nameRU;
+  const imageUrl = `https://api.nomoreparties.co/${movieData.image.url}`;
+  const getDuration = () => {
+    const hours = Math.floor(movieData.duration / 60);
+    const minutes = movieData.duration % 60;
+    return `${hours}ч ${minutes}м`;
   }
+
+  console.log(movieData)
 
   return (
     <li className='movies-card'>
       <SaveButton />
-      <img className='movies-card__image' src={moviesImage} alt={movie.title} />
+      <img className='movies-card__image' src={imageUrl} alt={title} />
       <div className='movies-card__description'>
-        <h3 className='movies-card__title'>{movie.title}</h3>
-        <p className='movies-card__duration'>{movie.duration}</p>
+        <h3 className='movies-card__title'>{title}</h3>
+        <p className='movies-card__duration'>{getDuration()}</p>
       </div>
     </li>
   )
